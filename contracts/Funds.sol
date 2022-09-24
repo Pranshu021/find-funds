00fa5bc0a4ecf6fe3187890c65d445038124d368
Pranshu021 <pranshu.duneja@wipro.com>
2022-09-25 01:13:07 +0530

    Adding solidity natspec comments to contracts

--- contracts/Funds.sol
+++ contracts/Funds.sol
@@ -2,38 +2,77 @@
 
 pragma solidity ^0.8.7;
 
+/// @title A decentralized Crowdfunding Platform
+/// @author Sting
+/// @dev Funds contract to hande fund transfers and manage balances, all the functions can only be called by Project contract.
 contract Funds {
     
     address private immutable owner;
-    address private projectPool = 0x0000000000000000000000000000000000000000;
+
+    /// @notice a projectContract variable is set to 0 at first
+    /// @dev This variable will be set afterward by the owner via function. 
+    address private projectContract = 0x0000000000000000000000000000000000000000;
+
+    /// @dev balances mapping to keep track of donation balance of each project registered and contributed to
     mapping(address => uint256) private balances;
+
+    /// @dev contriubution mapping of contributor address to project address to the amount.
+    /// To keep track of contributions made by a user to a project.
     mapping(address => mapping(address => uint256)) private contribution;
+
+    /// @dev _NOT_ENTERED, _ENTERED, and _status - variables used to check re-entrance in fund transfer calls. Booleans can also 
+    /// be used but more likelihood to receive full refund. For more explanation of this :
+    /// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/561d0eead30d3977847f2c2b4ae54eac31c218df/contracts/security/ReentrancyGuard.sol#L29
     uint256 private constant _NOT_ENTERED = 1;
     uint256 private constant _ENTERED = 2;
     uint256 private _status;
 
-    modifier onlyProjectPool {
-        require(msg.sender == projectPool, "ACCESS DENIED");
+
+    /// @dev modifier to check if the caller is Project contract or not.
+    modifier onlyProjectContract {
+        require(msg.sender == projectContract, "ACCESS DENIED");
         _;
     }
 
+    /// @notice _status is set to _NOT_ENTERED initially
     constructor() payable {
         owner = msg.sender;
         _status = _NOT_ENTERED;
     }
 
-    function setProjectPoolAddress(address _projectPoolAddress) external {
+    /**
+    * @dev function setProjectPool - sets the project contract address, can only be called by the deployer
+    * @notice projectContract address being passed after Project contract is deployed and initial 0 value is removed.
+    */
+    function setProjectContractAddress(address _projectContractAddress) external {
         require(msg.sender == owner, "ACCESS DENIED");
-        projectPool = _projectPoolAddress;
+        projectContract = _projectContractAddress;
     }
 
-    function contribute(address _contributorAddress, address _projectAddress) external payable onlyProjectPool {
+    /**
+    * @dev funciton contribute - accepts the investment money from the contributor and locks it. Request coming
+    * from Project contract
+    * @param _contributorAddress - address of the contributor/investor. Mostly msg.sender.
+    * @param _projectAddress - address of the project to contribute to.
+    * @dev Updates the balances, and contribution mapping.
+    */
+    function contribute(address _contributorAddress, address _projectAddress) external payable onlyProjectContract {
         balances[_projectAddress] += msg.value;
         contribution[_contributorAddress][_projectAddress] += msg.value;
     }
 
-    function refund(address _contributorAddress, address _projectAddress) external onlyProjectPool {
+    /**
+    * @dev function refund - To send the full contribution from a contributor/investor for a project.
+    * @notice _status value changes to _ENTERED as soon as refund is called, if someone tries to re-enter the _status != _ENTERED
+    * check fails and avoids any re-entrancy. If the transaction was normal and non-malicious, it proceeds and sets _status to
+    * _NOT_ENTERED again.
+    * @param _contributorAddress - address of contributor to give refund to - msg.sender
+    * @param _projectAddress - address of project to get refund from.
+    * @dev resets contribution mapping of contributor to 0, balance for project is reduced by the amount refunded.
+    */
+    function refund(address _contributorAddress, address _projectAddress) external onlyProjectContract {
         require(_status != _ENTERED, "Re-enterant call. YOU FRAUD !!");
+        _status = _ENTERED;
         bool _sent = payable(_contributorAddress).send(contribution[_contributorAddress][_projectAddress]);
         require(_sent, "Transaction failed");
         contribution[_contributorAddress][_projectAddress] = 0;
@@ -41,21 +80,24 @@ contract Funds {
         _status = _NOT_ENTERED;
     }
 
-    function getContributionInfo(address _contributorAddress, address _projectAddress) external view onlyProjectPool returns (uint256) {
-        return contribution[_contributorAddress][_projectAddress];
-    }
-
-    function getBalance(address _projectAddress) external view onlyProjectPool returns (uint256) {
-        return balances[_projectAddress];
-    }
-
+    /** 
+    * @dev function release - To release the funds collected from all the crowdfunding.
+    * @param _owner - Owner of the project, amount sent to the owner.
+    * @param _target - target of the project, used to validate if target is completed or not
+    * @param _releaseTime - Release time of project, used in validation.
+    * @param _votes - Number of votes to release funds at the time.
+    
+    * @dev Strict validations done before releasing funds - now time should be > releaseTime (90 days after registering)
+    * balance of project > set target, atleast 51% yes/true votes required to release the funds.
+    * balance mapping reset.
+    */
     function release(
         address _projectAddress, 
         address _owner, 
         uint256 _target, 
         uint256 _releaseTime,
         uint256 _votes,
-        uint256 _numberOfInvestors) external onlyProjectPool 
+        uint256 _numberOfInvestors) external onlyProjectContract 
         {
         require(block.timestamp >= _releaseTime, "Unable to release funds. Lock-in period not over");
         require(balances[_projectAddress] >= _target, "Target not reached yet");
@@ -65,6 +107,18 @@ contract Funds {
         balances[_projectAddress] = 0;
         }
 
+
+    /// @dev Returns amount of contribution made by a user to project.
+    function getContributionInfo(address _contributorAddress, address _projectAddress) external view onlyProjectContract returns (uint256) {
+        return contribution[_contributorAddress][_projectAddress];
+    }
+
+    /// @dev returns current balance of any project or amount of funds raised for any project
+    function getBalance(address _projectAddress) external view onlyProjectContract returns (uint256) {
+        return balances[_projectAddress];
+    }
+
+    /// falback function
     receive() external payable {}
  
 
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,6 +2,12 @@
 
 pragma solidity ^0.8.7;
 
+/// @title A decentralized Crowdfunding Platform
+/// @author Sting
+/// @dev Project.sol is the main contract, Funds contract can only be called by Project
+
+
+/// @dev Interface for Funds contract
 interface IFunds {
     function contribute(address _contributorAddress, address _projectAddress) external payable;
     function getContributionInfo(address _contributorAddress, address _projectAddress) external view returns (uint256);
@@ -15,7 +21,12 @@ contract Project {
     uint256 private projectIdCounter;
     uint256 private numberOfProjects;
     address private immutable owner;
-
+    /**
+    * @dev Structure of information about "project"
+    * projectAddress is required in case if project has a different address than the user
+    * release time is now + 90 days - time at which funds can be released
+    * target is the target amount that project dezires to fund from the crowd
+    */
     struct project {
         uint256 projectId;
         string name;
@@ -28,24 +39,39 @@ contract Project {
         uint256 votes;
     }
 
+    /// @dev mapping for an address to project
     mapping(address => project) private projectInfo;
     mapping(address => mapping(address => bool)) private vote;
 
+    /// @dev list of projects being registered on the contract
     project[] private projectList;
 
-    event projectCreated(string _name, string _description, address _address, address _owner, uint256 _target);
-    event projectRemoved(string _name, string _description, address _address, address _owner, uint256 _target);
-
+    event projectCreated(string _name, string _description, address _projectAddress, uint256 _releaseTime, uint256 _target, address _owner, uint256 _totalInvestors, uint256 _votes);
+    event projectRemoved(string _name, string _description, address _projectAddress, address _owner, uint256 _target);
+    event contributionEvent(address _projectAddress, address _contributorAddress, uint256 _amount);
+    event voteToggle(address _projectAddress, address _contributorAddress, bool _toggle);
+    event refundContributionEvent(address _projectAddress, address _contrinutorAddress);
+    event releaseFundsEvent(address _projectAddress, address _owner, uint256 _target, uint256 _releaseTime, uint256 _votes, uint256 _totalInvestors);
+    
+    /// @dev modifier to check if the mentioned project with the address exists or not.
+    /// @param _projectAddress - address of the project
     modifier projectExists(address _address) {
         require(projectInfo[_address].projectId != 0,  "No project exists with given address");
         _; 
     }
 
+    /**
+    * @dev modifier to confirm if the given user address (mostly msg.sender) is an investor to the project or not.
+    * @param _contributorAddress - Address of the investor/contributor to be confirmed
+    * @param _projectAddress - address of the project
+    */
     modifier contributor(address _contributorAddress, address _projectAddress) {
         require(fundContract.getContributionInfo(_contributorAddress, _projectAddress) > 0, "You have not contributed to this project");
         _;
     }
 
+    /// @dev activities like Releasing funds that can be only performed by the owner of the project
+    /// @param _projectAddress - address of the project.
     modifier onlyProjectOwner(address _projectAddress) {
         require(msg.sender == projectInfo[_projectAddress].owner, "Only Owner of the Project can call this");
         _;
@@ -56,27 +82,57 @@ contract Project {
         fundContract = IFunds(_fundContractAddress);
     }
 
+    /**
+    * @dev addProject function to create/add a project to start gathering funds.
+    * @param _name - name of the project
+    * @param _description - a small description about the project
+    * @param _address - address of the project, can be msg.sender or any other address
+    * @param _target - to specify a set amount to be achieved from this fund riser
+
+    * @dev
+    * Checks if any project already exists with the given address, and the address shouldn't be a 0 address, NO BURNING !!
+    * Creates a project struct with a unique id (not random) but sequential, releaseTime is set to 90 days from now
+    * Push the project to list of projects, increase the projectId counter and numberOfProject counter
+    */
     function addProject(string memory _name, string memory _description, address _address, uint256 _target) public {
         require(projectInfo[_address].projectId == 0, "Address already being used by another Project");
         require(_address != address(0), "Invalid address");
 
-        projectInfo[_address] = project(projectIdCounter+1, _name, _description, _address, (block.timestamp + 60 seconds), _target, msg.sender, 0, 0);
+        projectInfo[_address] = project(projectIdCounter+1, _name, _description, _address, (block.timestamp + 90 days), _target, msg.sender, 0, 0);
         projectList.push(projectInfo[_address]);
         projectIdCounter++;
         numberOfProjects++;
 
-        emit projectCreated(projectInfo[_address].name, projectInfo[_address].description, _address, projectInfo[_address].owner, projectInfo[_address].target);
+        emit projectCreated(projectInfo[_address].name, projectInfo[_address].description, _address, projectInfo[_address].releaseTime, projectInfo[_address].target, projectInfo[_address].owner, projectInfo[_address].totalInvestors, projectInfo[_address].votes);
         
     }
 
+    /**
+    * @dev removeProject Function to remove a project, this function can only be called by project Owner and can be called manually or is called automatically after releaseFunds
+    * If balance of project is > 0, the project cannot be removed as it still holds the funds that are invested by users
+    * @param _address - Address of the project to be removed
+    * @dev project is set to 0 in the list, using the sequential index being stored in parallel with id. Mapping is deleted.
+    */
     function removeProject(address _address) public projectExists(_address) onlyProjectOwner(_address) {
         projectList[(projectInfo[_address].projectId) - 1] = project(0, '', '', address(0), 0, 0, address(0), 0, 0);
         numberOfProjects--;
         emit projectRemoved(projectInfo[_address].name, projectInfo[_address].description, _address, projectInfo[_address].owner, projectInfo[_address].target);
         delete projectInfo[_address];
     }
+    /**
+    * @dev function contributeToProject - For any user to contribute to a project by mentioning the address.
+    * @param _projectAddress - Address of the project to invest funds to
+    * @dev Fetching the contribution from Funds contract to confirm if investing first time in the mentioned project. This is 
+    * used in setting the intial vote
+    * Initially, by default when anyone contributes/invest in any project, the vote to release the funds by owner is TRUE but
+    * a button and warning is shown on front end to toggle it, this is done for the scenario where if majority of stakeholders are
+    * unable to vote true (forgot keys to account, forgot to give vote, etc.) and the funds getting stuck in
+    * the contract rendering them useless.
 
-    function contributeToProject(address _projectAddress) external payable projectExists(_projectAddress) {
+    * @dev since fund transfers and "banking" functions are handled by Funds contract, request along with msg.value is sent to
+    * create an entry in the ledger as a contribution and increase the balance of mentioned project. 
+    */
+    function contributeToProject(address _projectAddress) public payable projectExists(_projectAddress) {
         require(msg.value > 0, "Amount should be greater than 0");
         uint256 contribution = fundContract.getContributionInfo(msg.sender, _projectAddress);
         fundContract.contribute{value: msg.value}(msg.sender, _projectAddress);
@@ -86,9 +142,21 @@ contract Project {
             projectInfo[_projectAddress].votes += 1;
             vote[msg.sender][_projectAddress] = true;
         }
+
+        emit contributionEvent(_projectAddress, msg.sender, msg.value);
     }
 
-    function releaseFunds(address _projectAddress) external projectExists(_projectAddress) onlyProjectOwner(_projectAddress) {
+    /**
+    * @dev function releaseFunds - Function that can be only called by project owners, to release the collected funds from Funds
+    * (The "Bank") contract to their or projects accounts, to further utilize them
+    * @param _projectAddress - Address of the project to release funds from
+    * @dev the required checks for voting and releaseTime are done by Funds contract. 
+    * release function from fundsContract is called to do the required checks, send the amount to the owner/project if all checks
+    * pass, update the ledger and balances.
+    * Project is removed after funds are released, owner can again start with the same address or fresh address with new target if
+    * more funds are required.
+    */
+    function releaseFunds(address _projectAddress) public projectExists(_projectAddress) onlyProjectOwner(_projectAddress) {
         fundContract.release(_projectAddress, 
         projectInfo[_projectAddress].owner, 
         projectInfo[_projectAddress].target, 
@@ -96,44 +164,72 @@ contract Project {
         projectInfo[_projectAddress].votes,
         projectInfo[_projectAddress].totalInvestors);
 
+        emit releaseFundsEvent(_projectAddress, projectInfo[_projectAddress].owner, projectInfo[_projectAddress].target, projectInfo[_projectAddress].releaseTime, projectInfo[_projectAddress].votes, projectInfo[_projectAddress].totalInvestors);
+
         removeProject(_projectAddress);
-        
     }
 
-    function toggleVote(address _projectAddress) external projectExists(_projectAddress) contributor(msg.sender, _projectAddress) {
+    /**
+    * @dev function toggleVote - To toggle the vote that controls condition of releasing funds, from default true to false or
+    * false to true, based on previous value.
+    * @param _projectAddress - Address of the project to vote in regards to
+    */
+    function toggleVote(address _projectAddress) public projectExists(_projectAddress) contributor(msg.sender, _projectAddress) {
         if(vote[msg.sender][_projectAddress]) {
             vote[msg.sender][_projectAddress] = false;
             projectInfo[_projectAddress].votes -= 1;
+            emit voteToggle(_projectAddress, msg.sender, false);
+
         } else {
             vote[msg.sender][_projectAddress] = true;
             projectInfo[_projectAddress].votes += 1;
+            emit voteToggle(_projectAddress, msg.sender, true);
         }
     }
-
-    function refundContribution(address _projectAddress) external projectExists(_projectAddress) contributor(msg.sender, _projectAddress) {
+    /**
+    * @dev function refundContribution - to get a full refund of the contribution made.
+    * @param _projectAddress - Address of the project to get a refund from
+    * @dev calling refund function from Funds contract, a necessary check already done to validate if the user has contributed.
+    */
+    function refundContribution(address _projectAddress) public projectExists(_projectAddress) contributor(msg.sender, _projectAddress) {
         fundContract.refund(msg.sender, _projectAddress);
+        emit refundContributionEvent(_projectAddress, msg.sender);
     }
 
+    /// @dev returns the struct of project from the address it's mapped to.
+    /// @param _address - address of project to get info about.
     function getProject(address _address) external view projectExists(_address) returns (project memory) {
         return projectInfo[_address];
     }
 
+    /// @dev returns total number of projects registered.
     function getTotalNumberofProjects() external view returns (uint256) {
         return numberOfProjects;
     }
 
+    /// @dev returns the address of owner of project, can be project address itself.
+    /// @param address - address of project to get owner info about.
     function getOwnerofProject(address _address) external view projectExists(_address) returns (address)  {
         return projectInfo[_address].owner;
     }
 
+    /** 
+    * @dev returns the amount of investment made by a user to mentioned project.
+    * Calls Fund contract get mapping function to retreive the ledger info.
+    * @param _contributorAddres - Address of user who contributed, can be msg.sender
+    * @param _projectAddress - address of project to validate investment made to. 
+    */
     function getContribution(address _contributorAddress, address _projectAddress) external view projectExists(_projectAddress) contributor(_contributorAddress, _projectAddress) returns (uint256) {
         return fundContract.getContributionInfo(_contributorAddress, _projectAddress);
     }
 
+    /** 
+    * @dev returns the vote to release funds is true or false for a user to the mentioned project.
+    * @param _contributor - address of user
+    * @param _projectAddress - address of the project
+    */
     function getVotingInfo(address _contributor, address _projectAddress) external view projectExists(_projectAddress) contributor(_contributor, _projectAddress) returns (bool) {
         return vote[_contributor][_projectAddress];
     }
 
 }
-
-// Decrease the votes in toggleVote function
